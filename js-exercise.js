14a59028c354242fd9e124bfacba2aaa3e1acdf2
Carlos Allende <carlosallendesanz@gmail.com>
2016-03-10 19:05:49 -0800

    js-lab done

--- js-exercise.js
+++ js-exercise.js
@@ -119,12 +119,12 @@ var theTarget = Math.round(100 * Math.random()) + 1;
 ​
 var guess = 0;
 var theTarget = Math.round(100 * Math.random()) + 1;
-while (null) {
+while (guess !== theTarget) {
     guess = prompt("Make a guess!");
 ​
-    if (null) {
+    if (guess > theTarget) {
         alert("Too high!");
-    } else if (null) {
+    } else if (guess < theTarget) {
         alert("Too low!");
     }
 }
@@ -134,13 +134,181 @@ while (null) {
  "count up" clock. Make it console.log the
  numbers 1, 2, 3, 4, and 5. */
 ​
-var i = null;
-while (i < null) {
-    console.log(null);
-    null;
+var i = 1;
+while (i < 6) {
+    console.log(i);
+    i++;
+}
+
+​/************************
+ *  FOR LOOPS
+ ************************/
+​
+/* Rewrite the "count up" clock as a for loop! */
+​
+for (var i = 0; i<6; i++) {
+    console.log(i);
+}
+​
+/* Rewrite the for loop to have no initial conditions. */
+​
+var i = 1;
+for (; i < 6; i++) {
+    console.log(i);
+}
+​
+/* Rewrite the for loop to have no incrementing statement. */
+​
+for (i=1; i<6; ) {
+    console.log(i);
+    i++;
+}
+​
+/* Rewrite the for loop to have no incrementing or intial
+ statements. */
+​
+var i=1;
+for (; i<6; ) {
+    console.log(i);
+    i++;
+}
+​
+/* Note that this is identical to the while loop! */
+​
+​
+/* Let's make a count DOWN clock. We will display
+ 5, 4, 3, 2, 1, 0. */
+​
+var i = 5;
+while (i >= 0) {
+    console.log(i);
+    i--;
+}
+​
+/* Now, rewrite the countdown clock as a for loop! */
+​
+for (var i = 5; i>=0; i--) {
+    console.log(i);
 }
 ​
 ​
+/* Using a for loop, print each value of the array
+ to the console. */
+​
+var foods = ["Burger", "Fries", "Drink", "Tomato"];
+​
+for (i=0; i<foods.length; i++) {
+    console.log(foods[i]);
+}
+​
+/* Using a for loop, print each value of the array
+ to the console, backwards! */
+​
+var foods = ["Burger", "Fries", "Drink", "Tomato"];
+​
+for (i=foods.length-1; i>=0; i--) {
+    console.log(foods[i]);
+}
+​
+​
+​
+/************************
+ *  FUNCTIONS
+ ************************/
+​
+/* Given the following function, call the function with
+ appropriate example values. In a comment, write
+ what the expected output will be. */
+​
+function concatStrings(str, appendString) {
+    return str + appendString;
+}
+​
+concatStrings("hello","world");  // output is: helloworld
+​
+​
+/* Write a function that takes one number as a parameter,
+ and returns that number plus one. Choose good
+ variable names! Call the function with an appropriate
+ parameter. */
+​
+function plusOne(a) {
+    return a+1;
+}
+​
+plusOne(4);  // output is: 5
+​
+​
+/* Complete the below function. It accepts a
+ number and returns a string with "a" repeated
+ that many times. For example, calling the
+ function with 3 will return "aaa".
+ Use a for loop to construct the return
+ string. Call the function
+ with example values and write the output as
+ a comment. */
+​
+function repeatA(numTimesRepeated) {
+    var returnString = "";
+    for (i=0; i<numTimesRepeated; i++) {
+        returnString += "a"
+    }
+    return returnString;
+}
+​
+repeatA(3);   // output is: aaa
+​
+​
+/* Let's generalize the last function. Now, a
+ second parameter will be passed which is the
+ character (or string) to repeat! Call the function
+ with example values and write the output as
+ a comment. */
+​
+function repeatString(numTimesRepeated, stringToRepeat) {
+    for (i=0;i<numTimesRepeated; i++) {
+    return ("" += stringToRepeat)// modify the last function's code
+}
+}
+​
+repeatString(4, "b");  // output is: bbbb
+​
+​
+/* Write a function which returns the original array,
+ but reversed! */
+​
+function reverseStrings(arrayOfStrings)
+{
+    var reversedArray = [];
+​
+    for (i=arrayOfStrings.length-1; i<arrayOfStrings.length; i++) {
+        reversedArray.push(i);
+    }
+​
+    return reversedArray;
+}
+​
+var foods = ["Burger", "Fries", "Drink", "Tomato"];
+reverseStrings(foods);
+​
+/* Note that variables can be assigned functions. */
+​
+var someVariable = repeatA;
+var anotherVariable = repeatA;
+​
+/* Try running the above two lines in your browser.
+ Now, try calling the functions by using the
+ new variables. (i.e. run the below code!) */
+​
+someVariable(3);
+anotherVariable(5);
+​
+/* This is an important fact about JavaScript --
+ functions can be assigned to variables and
+ passed around as arguments to functions! */
+​
+​
+/* CONGRATULATIONS! YOU MADE IT TO THE END! */
 
 
 
